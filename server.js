90b8f50bb8be95708ef94fdc8d31ff47f9353108
Barry25000 <barryengler@gmail.com>
2023-06-21 21:06:15 -0500

    server update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
 //connect to database
 const db = mysql.createConnection(
@@ -64,29 +64,98 @@ var viewAllDepartments = () => {
   options();
 };
 var viewAllRoles = () => {
-  db.query("SELECT * FROM role", function (err, results) {
+  const sql = `SELECT role.id, title, salary, department.name FROM role
+  inner join department on department.id=role.department_id
+  `;
+
+  db.query(sql, function (err, results) {
     console.log(results);
+    console.log(err);
   });
   options();
 };
 
 var viewAllEmployees = () => {
-  db.query("SELECT * FROM employee", function (err, results) {
+  const sql = `SELECT employee.id AS employee_id, employee.first_name AS first_name, employee.last_name AS last_name, role.title, department.name, role.salary, manager.first_name AS manager_name FROM employee
+    left outer join role on role.id=employee.role_id
+    left outer join department on department.id=role.department_id
+    left outer join employee manager on manager.id=employee.manager_id
+    `;
+
+  db.query(sql, function (err, results) {
     console.log(results);
+    console.log(err);
   });
   options();
 };
 
 var addDepartment = () => {
-  db.insert("SELECT * FROM department", function (err, results) {
-    console.log(results);
-  });
-  options();
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the new department?",
+        name: "department",
+      },
+    ])
+    .then((answer) => {
+      db.query(
+        "INSERT INTO department (name) VALUES (?)",
+        [answer.department],
+        function (error, results, fields) {
+          if (error) console.log(error);
+          console.log(results.insertId);
+          options();
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
-
 var addRole = () => {
-  console.log("hello4");
-  options();
+  db.query("SELECT * FROM department", function (err, results) {
+    console.log(results);
+    const departmentName = results.map((department) => department.name);
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the name of the new role",
+          name: "role",
+        },
+        {
+          type: "number",
+          message: "What is the salary of this role?",
+          name: "salary",
+        },
+        {
+          type: "list",
+          message: "What department will this role belong to?",
+          choices: departmentName,
+          name: "department",
+        },
+      ])
+      .then((answer) => {
+        console.log(answer);
+        const departmentId = results.find(
+          (b) => b.name === answer.department
+        ).id;
+        db.query(
+          "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)",
+          [answer.role, answer.salary, departmentId],
+          function (error, results, fields) {
+            if (error) console.log(error);
+            console.log(results.insertId);
+            options();
+          }
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 };
 
 var addEmployee = () => {
